3fe1264dd25bd7bf2d192ac3f8bcef2fe8b336a7
Shirley <illangayo00@gmail.com>
2022-12-07 01:11:13 +0900

    컴포넌트 합성

--- follow/src/App.js
+++ follow/src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
 }
 
 export default App;
