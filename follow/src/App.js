3ec6db48069ba5f8ed012b2cc7a22322cc9371b4
Shirley <illangayo00@gmail.com>
2022-12-07 01:35:04 +0900

    컴포넌트 추출연습

--- follow/src/App.js
+++ follow/src/App.js
@@ -1,15 +1,63 @@
 import './App.css';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64',
+  },
+};
+
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function UserName(props) {
+  return <div className="UserInfo-name">{props.user.name}</div>;
+}
+
+function UserInfo(props) {
+  return (
+    <div className="userInfo">
+      <Avatar user={props.user} />
+      <UserName user={props.user} />
+    </div>
+  );
+}
+
+function Text(props) {
+  return <div className="Comment-text">{props.text}</div>;
+}
+
+function Date(props) {
+  return <div className='"Comment-date'>{formatDate(props.date)}</div>;
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <Text text={props.text} />
+      <Date date={props.date} />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      />
     </div>
   );
 }
--- follow/src/index.js
+++ follow/src/index.js
@@ -5,25 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = <h1>Hello, world</h1>;
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
 
-  root.render(element);
-}
-
-setInterval(tick, 1000);
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//     {element}
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
